36747070c8a38e6ecc094600287c6eb028a9973d
Ali Sartaz Khan <71156712+alisartazkhan@users.noreply.github.com>
2022-10-01 22:42:26 -0700

    Update standard_ships.py

--- standard_ships.py
+++ standard_ships.py
@@ -1,26 +1,23 @@
-"""Standard ship types for the Battleship game.
-   Defines a function for each of the well-known ship types in Battleship;
-   the function creates a new Ship object every time that it is called.
-   The code in this module builds a standard shape, which always includes
-   the point (0,0); the caller passes these functions a 'rot' field, which
-   is used to rotate the shape before we pass it to the Ship constructor.
+'''
+Standard ship types for the Battleship game.
+Defines a function for each of the well-known ship types in Battleship;
+the function creates a new Ship object every time that it is called.
+The code in this module builds a standard shape, which always includes
+the point (0,0); the caller passes these functions a 'rot' field, which
+is used to rotate the shape before we pass it to the Ship constructor.
 
-   Primary functions:
-      Carrier   (rot)
-      Battleship(rot)
-      Cruiser   (rot)
-      Submarine (rot)
-      Destroyer (rot)
+Primary functions:
+   Carrier   (rot)
+   Battleship(rot)
+   Cruiser   (rot)
+   Submarine (rot)
+   Destroyer (rot)
 
-   Utility functions:
-      rotate_shape(shape,rot)
-"""
-
-
+Utility functions:
+   rotate_shape(shape,rot)
+'''
 
 from battleship import Ship
-
-
 
 def Carrier(rot):
     retval = Ship("Aircraft Carrier", [(0,0), (1,0), (2,0), (3,0), (4,0)] )
@@ -46,5 +43,3 @@ def Destroyer(rot):
     retval = Ship("Destroyer", [(0,0), (1,0)] )
     retval.rotate(rot)
     return retval
-
-
